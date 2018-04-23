7e24bea1bc1209be0557d5d61740842d35356977
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel.Design;
+using DevExpress.XtraReports.UserDesigner.Native;
+using DevExpress.XtraReports.UI;
+using System.IO;
+
+namespace RepLivePreview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XtraReport1 report = new XtraReport1();
+
+            report.DataSource = ManualDataSet.CreateData();
+
+            xrDesignPanel1.OpenReport(report);
+            xrDesignPanel1.SetCommandVisibility(DevExpress.XtraReports.UserDesigner.ReportCommand.ShowPreviewTab, DevExpress.XtraReports.UserDesigner.CommandVisibility.None);
+
+            IDesignerHost dh = xrDesignPanel1.GetService(typeof(IDesignerHost)) as IDesignerHost;
+            dh.TransactionClosed += new DesignerTransactionCloseEventHandler(dh_TransactionClosed);
+
+            UpdateView();
+        }
+
+        void dh_TransactionClosed(object sender, DesignerTransactionCloseEventArgs e) {
+            UpdateView();
+        }
+
+        void UpdateView() {
+            XtraReport report = xrDesignPanel1.Report;
+            XtraReport report2 = new XtraReport();
+            MemoryStream ms = new MemoryStream();
+            
+            report.SaveLayout(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+            report2.LoadLayout(ms);
+            report2.DataSource = report.DataSource;
+            report2.CreateDocument();
+
+            printControl1.PrintingSystem = report2.PrintingSystem;
+            printControl1.ExecCommand(DevExpress.XtraPrinting.PrintingSystemCommand.ZoomToWholePage);
+        }
+
+    }
+
+    public class ManualDataSet : DataSet {
+        public ManualDataSet()
+            : base() {
+            DataTable table = new DataTable("table");
+
+            DataSetName = "ManualDataSet";
+
+            table.Columns.Add("ID", typeof(Int32));
+            table.Columns.Add("MyDateTime", typeof(DateTime));
+            table.Columns.Add("MyRow", typeof(string));
+            table.Columns.Add("MyData", typeof(double));
+            table.Constraints.Add("IDPK", table.Columns["ID"], true);
+
+            Tables.AddRange(new DataTable[] { table });
+        }
+
+        public static ManualDataSet CreateData() {
+            ManualDataSet ds = new ManualDataSet();
+            DataTable table = ds.Tables["table"];
+
+            table.Rows.Add(new object[] { 0, DateTime.Today, "A", 103 });
+            table.Rows.Add(new object[] { 1, DateTime.Today, "B", 200 });
+            table.Rows.Add(new object[] { 2, DateTime.Today, "C", 446 });
+            table.Rows.Add(new object[] { 3, DateTime.Today.AddDays(1), "A", 788 });
+            table.Rows.Add(new object[] { 4, DateTime.Today.AddDays(1), "B", 787 });
+            table.Rows.Add(new object[] { 5, DateTime.Today.AddDays(1), "C", 452 });
+            table.Rows.Add(new object[] { 6, DateTime.Today.AddDays(2), "A", 152 });
+            table.Rows.Add(new object[] { 7, DateTime.Today.AddDays(2), "B", 565 });
+            table.Rows.Add(new object[] { 8, DateTime.Today.AddDays(2), "C", 612 });
+
+            return ds;
+        }
+
+        #region Disable Serialization for Tables and Relations
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public new DataTableCollection Tables {
+            get { return base.Tables; }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public new DataRelationCollection Relations {
+            get { return base.Relations; }
+        }
+        #endregion Disable Serialization for Tables and Relations
+    }
+
+}
---
+++ CS/XtraReport1.Designer.cs
@@ -0,0 +1,160 @@
+namespace RepLivePreview
+{
+    partial class XtraReport1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.PageFooter = new DevExpress.XtraReports.UI.PageFooterBand();
+            this.ReportHeader = new DevExpress.XtraReports.UI.ReportHeaderBand();
+            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
+            this.manualDataSet1 = new RepLivePreview.ManualDataSet();
+            this.xrLabel2 = new DevExpress.XtraReports.UI.XRLabel();
+            this.xrLabel3 = new DevExpress.XtraReports.UI.XRLabel();
+            this.xrLabel4 = new DevExpress.XtraReports.UI.XRLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.manualDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // Detail
+            // 
+            this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrLabel4,
+            this.xrLabel3,
+            this.xrLabel2});
+            this.Detail.Height = 25;
+            this.Detail.Name = "Detail";
+            this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // PageFooter
+            // 
+            this.PageFooter.Height = 30;
+            this.PageFooter.Name = "PageFooter";
+            this.PageFooter.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // ReportHeader
+            // 
+            this.ReportHeader.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrLabel1});
+            this.ReportHeader.Height = 42;
+            this.ReportHeader.Name = "ReportHeader";
+            // 
+            // xrLabel1
+            // 
+            this.xrLabel1.Font = new System.Drawing.Font("Times New Roman", 25F);
+            this.xrLabel1.Location = new System.Drawing.Point(275, 0);
+            this.xrLabel1.Name = "xrLabel1";
+            this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
+            this.xrLabel1.Size = new System.Drawing.Size(100, 42);
+            this.xrLabel1.StylePriority.UseFont = false;
+            this.xrLabel1.Text = "Report";
+            // 
+            // manualDataSet1
+            // 
+            this.manualDataSet1.DataSetName = "ManualDataSet";
+            // 
+            // xrLabel2
+            // 
+            this.xrLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.xrLabel2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.xrLabel2.Borders = ((DevExpress.XtraPrinting.BorderSide)((((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Top)
+                        | DevExpress.XtraPrinting.BorderSide.Right)
+                        | DevExpress.XtraPrinting.BorderSide.Bottom)));
+            this.xrLabel2.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "table.MyData", "")});
+            this.xrLabel2.Location = new System.Drawing.Point(408, 0);
+            this.xrLabel2.Name = "xrLabel2";
+            this.xrLabel2.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
+            this.xrLabel2.Size = new System.Drawing.Size(100, 25);
+            this.xrLabel2.StylePriority.UseBackColor = false;
+            this.xrLabel2.StylePriority.UseBorderColor = false;
+            this.xrLabel2.StylePriority.UseBorders = false;
+            this.xrLabel2.Text = "xrLabel2";
+            // 
+            // xrLabel3
+            // 
+            this.xrLabel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.xrLabel3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.xrLabel3.Borders = ((DevExpress.XtraPrinting.BorderSide)((((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Top)
+                        | DevExpress.XtraPrinting.BorderSide.Right)
+                        | DevExpress.XtraPrinting.BorderSide.Bottom)));
+            this.xrLabel3.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "table.MyDateTime", "")});
+            this.xrLabel3.Location = new System.Drawing.Point(92, 0);
+            this.xrLabel3.Name = "xrLabel3";
+            this.xrLabel3.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
+            this.xrLabel3.Size = new System.Drawing.Size(100, 25);
+            this.xrLabel3.StylePriority.UseBackColor = false;
+            this.xrLabel3.StylePriority.UseBorderColor = false;
+            this.xrLabel3.StylePriority.UseBorders = false;
+            this.xrLabel3.Text = "xrLabel3";
+            // 
+            // xrLabel4
+            // 
+            this.xrLabel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.xrLabel4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.xrLabel4.Borders = ((DevExpress.XtraPrinting.BorderSide)((((DevExpress.XtraPrinting.BorderSide.Left | DevExpress.XtraPrinting.BorderSide.Top)
+                        | DevExpress.XtraPrinting.BorderSide.Right)
+                        | DevExpress.XtraPrinting.BorderSide.Bottom)));
+            this.xrLabel4.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "table.MyRow", "")});
+            this.xrLabel4.Location = new System.Drawing.Point(242, 0);
+            this.xrLabel4.Name = "xrLabel4";
+            this.xrLabel4.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
+            this.xrLabel4.Size = new System.Drawing.Size(100, 25);
+            this.xrLabel4.StylePriority.UseBackColor = false;
+            this.xrLabel4.StylePriority.UseBorderColor = false;
+            this.xrLabel4.StylePriority.UseBorders = false;
+            this.xrLabel4.Text = "xrLabel4";
+            // 
+            // XtraReport1
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.Detail,
+            this.PageFooter,
+            this.ReportHeader});
+            this.DataMember = "table";
+            this.DataSource = this.manualDataSet1;
+            this.Version = "8.2";
+            ((System.ComponentModel.ISupportInitialize)(this.manualDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraReports.UI.DetailBand Detail;
+        private DevExpress.XtraReports.UI.PageFooterBand PageFooter;
+        private DevExpress.XtraReports.UI.ReportHeaderBand ReportHeader;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel1;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel4;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel3;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel2;
+        private ManualDataSet manualDataSet1;
+    }
+}
